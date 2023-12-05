7317dd3536f108423cb1fb59211489e9eddc5320
Harshalkatakiya <katakiyaharshl001@gmail.com>
2023-12-05 23:59:01 +0530

    useLocalStorage Hook added

--- src/utils/useContext.js
+++ src/utils/useContext.js
@@ -1,12 +1,13 @@
 'use client';
+import useLocalStorage from "use-local-storage";
 import { createContext, useContext, useEffect, useState } from 'react';
 import Toast from './react-toastify/toast';
 const UserContext = createContext();
 export const useUserData = () => useContext(UserContext);
 
 export function UserProvider({ children }) {
-    const [cartItems, setCartItems] = useState(JSON.parse(localStorage.getItem('cartItems')) || []);
-    useEffect(() => { localStorage.setItem('cartItems', JSON.stringify(cartItems)); }, [cartItems]);
+    const [cartItems, setCartItems] = useLocalStorage("cartItems", "");
+    //useEffect(() => { localStorage.setItem('cartItems', JSON.stringify(cartItems)); }, [cartItems]);
     const addItemToCart = (item) => {
         setCartItems((prevItems) => [...prevItems, item]);
         Toast('Item Added Successfully', 'success');
